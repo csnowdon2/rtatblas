321a5214462f464c6095a1197252d0dded7eccb2
Calum Snowdon <calumsnowdon549@gmail.com>
2024-04-15 13:13:56 +0800

    Removed autotune results from AD script

--- run_AD.sh
+++ run_AD.sh
@@ -3,18 +3,19 @@
 
 shapes=("960 324480 960" "120 2957880 120" "192 738048 192")
 reps=20
+DIR="./build/src/app/"
 
 echo "Running computations with ${reps} repetitions each"
 
 for shape in "${shapes[@]}"
 do
-  echo ${shape}
+  echo "( m k n ) = (" ${shape} ")"
   for options in "N N" "N T" "T N" "T T"
   do
     echo -n "${options}: "
-    ./build/src/app/simple_test ${shape} ${options} ${reps}
+    ${DIR}/simple_test ${shape} ${options} ${reps}
   done
-  echo -n "Autotuned: "
-  ./build/src/app/autotune ${shape} N N ${reps}
+  #echo -n "Autotuned: "
+  #./build/src/app/autotune ${shape} N N ${reps}
   echo
 done
